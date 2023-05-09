959ccb0f72f23cc7ed318efeba08081d5055ce0b
stord-engineering-account <77021775+stord-engineering-account@users.noreply.github.com>
2023-05-09 16:22:42 -0600

    chore(main): release 1.5.0 (#9)
    :robot: I have created a release *beep* *boop*
    ---
    
    ##
    [1.5.0](https://github.com/stordco/actions-sync/compare/v1.4.0...v1.5.0)
    (2023-05-09)
    
    ### Features
    
    * add license
    ([eb1f908](https://github.com/stordco/actions-sync/commit/eb1f908b8abf45acd29487f3cb6214b5f5dd226d))
    * allow running scripts and passing variables between
    ([cad4cc2](https://github.com/stordco/actions-sync/commit/cad4cc2ae3ec40ce84dfd77f155fc8505e1a5b90))
    * build files when releasing
    ([d4c4d1f](https://github.com/stordco/actions-sync/commit/d4c4d1f0b7da859bdb97a908c74635803c64026b))
    * setup PR workflow
    ([f18f7d2](https://github.com/stordco/actions-sync/commit/f18f7d27dd24837c4eca937fbe6f82ebeb440158))
    
    ### Bug Fixes
    
    * add files to git when running commitChanges
    ([b889ba0](https://github.com/stordco/actions-sync/commit/b889ba02165037b86b3deeed73238dbe4d1a29c7))
    * add release-please config
    ([0c5a99e](https://github.com/stordco/actions-sync/commit/0c5a99e965cdee3bbb471caa83845cfd4ad4fe59))
    * create folder for any file before templating
    ([9659e94](https://github.com/stordco/actions-sync/commit/9659e94e801b334da35355076a4db42fd66b0f8c))
    * more updates to release please
    ([#8](https://github.com/stordco/actions-sync/issues/8))
    ([0d33f1c](https://github.com/stordco/actions-sync/commit/0d33f1ce6591f34b8843d4aaf98d42ca4704a8b8))
    * override new release tag with built files
    ([f4a54f9](https://github.com/stordco/actions-sync/commit/f4a54f9c42eb0d3b4e9e0ce6b27d41d7af7e26d5))
    * update config loading names from camel case to dash case
    ([2fa99a2](https://github.com/stordco/actions-sync/commit/2fa99a26c8b8b7ee79d374a5327844aa0e761e25))
    * update GHA workflows
    ([e032156](https://github.com/stordco/actions-sync/commit/e032156eb3c9401e5a89ac18a5b4694e6e3cdb77))
    * update git commit logic
    ([2ed7907](https://github.com/stordco/actions-sync/commit/2ed7907edede06fdcc8fe2f2a767402597eb7b43))
    * update release build to amend with no edit
    ([c11a271](https://github.com/stordco/actions-sync/commit/c11a271bd9fb16131bdd1ca3d12456dd72a3a404))
    * update release please action commands
    ([#7](https://github.com/stordco/actions-sync/issues/7))
    ([786b160](https://github.com/stordco/actions-sync/commit/786b160b9208a9286a7e8e435dd917956da590ed))
    * update scripts output path file creation
    ([c220514](https://github.com/stordco/actions-sync/commit/c220514f2879356dbb18527e5288937d1ad2e9a7))
    * update sync-token to sync-auth in action.yml
    ([3e63e07](https://github.com/stordco/actions-sync/commit/3e63e07bd38374d99d5bfd870b9c8a23b917b46c))
    * update typescript esm imports not working after build
    ([5d9f148](https://github.com/stordco/actions-sync/commit/5d9f1483d6d5eea25a4ae60b74c242d963f542e2))
    
    ---
    This PR was generated with [Release
    Please](https://github.com/googleapis/release-please). See
    [documentation](https://github.com/googleapis/release-please#release-please).

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import * as core from "@actions/core";
+
+import { configureRepository, cloneRepository, commitChanges, createPr } from "./git";
+import { getConfig } from "./config";
+import { templateFiles } from "./templates";
+import { runScripts } from "./scripts";
+
+export async function run() {
+  const config = getConfig();
+
+  await configureRepository(config);
+  await cloneRepository(config);
+  await runScripts(config);
+  await templateFiles(config);
+
+  if (config.prEnabled) {
+    const hasChanges = await commitChanges(config);
+
+    if (hasChanges === false) {
+      core.info("No changes to commit.");
+      return;
+    }
+
+    core.info("Creating PR");
+    await createPr(config);
+    core.info("Created PR");
+  } else {
+    core.info("Skipping PR creation");
+  }
+}
