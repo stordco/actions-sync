ce0e8fcbe5201f7ce773e64af9c8bf2ca66c7e4b
stord-engineering-account <77021775+stord-engineering-account@users.noreply.github.com>
2023-05-09 13:10:55 -0600

    chore(main): release 1.3.0 (#5)
    :robot: I have created a release *beep* *boop*
    ---
    
    ##
    [1.3.0](https://github.com/stordco/actions-sync/compare/v1.2.0...v1.3.0)
    (2023-05-09)
    
    ### Features
    
    * add license
    ([eb1f908](https://github.com/stordco/actions-sync/commit/eb1f908b8abf45acd29487f3cb6214b5f5dd226d))
    * allow running scripts and passing variables between
    ([cad4cc2](https://github.com/stordco/actions-sync/commit/cad4cc2ae3ec40ce84dfd77f155fc8505e1a5b90))
    * build files when releasing
    ([d4c4d1f](https://github.com/stordco/actions-sync/commit/d4c4d1f0b7da859bdb97a908c74635803c64026b))
    * setup PR workflow
    ([f18f7d2](https://github.com/stordco/actions-sync/commit/f18f7d27dd24837c4eca937fbe6f82ebeb440158))
    
    ### Bug Fixes
    
    * add release-please config
    ([0c5a99e](https://github.com/stordco/actions-sync/commit/0c5a99e965cdee3bbb471caa83845cfd4ad4fe59))
    * create folder for any file before templating
    ([9659e94](https://github.com/stordco/actions-sync/commit/9659e94e801b334da35355076a4db42fd66b0f8c))
    * override new release tag with built files
    ([f4a54f9](https://github.com/stordco/actions-sync/commit/f4a54f9c42eb0d3b4e9e0ce6b27d41d7af7e26d5))
    * update config loading names from camel case to dash case
    ([2fa99a2](https://github.com/stordco/actions-sync/commit/2fa99a26c8b8b7ee79d374a5327844aa0e761e25))
    * update GHA workflows
    ([e032156](https://github.com/stordco/actions-sync/commit/e032156eb3c9401e5a89ac18a5b4694e6e3cdb77))
    * update release build to amend with no edit
    ([c11a271](https://github.com/stordco/actions-sync/commit/c11a271bd9fb16131bdd1ca3d12456dd72a3a404))
    * update scripts output path file creation
    ([c220514](https://github.com/stordco/actions-sync/commit/c220514f2879356dbb18527e5288937d1ad2e9a7))
    * update sync-token to sync-auth in action.yml
    ([3e63e07](https://github.com/stordco/actions-sync/commit/3e63e07bd38374d99d5bfd870b9c8a23b917b46c))
    * update typescript esm imports not working after build
    ([5d9f148](https://github.com/stordco/actions-sync/commit/5d9f1483d6d5eea25a4ae60b74c242d963f542e2))
    
    ---
    This PR was generated with [Release
    Please](https://github.com/googleapis/release-please). See
    [documentation](https://github.com/googleapis/release-please#release-please).

---
+++ src/entrypoint.ts
@@ -0,0 +1,19 @@
+import * as core from "@actions/core";
+
+import { run } from "./index";
+
+(async () => {
+  try {
+    await run();
+  } catch (error) {
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    } else if (typeof error === "string") {
+      core.setFailed(error);
+    } else {
+      core.setFailed("Unknown error occurred");
+    }
+
+    process.exit(1);
+  }
+})();
